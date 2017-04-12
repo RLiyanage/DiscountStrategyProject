3f8db67f251943675df076c3c1a266e65cdba6a0
Sanuth <Sanuth@192.168.2.12>
2017-04-12 09:30:46 -0500

    Added unit testing to Discount Strategy project

---
+++ test/discountstrategyproject/ProductTest.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package discountstrategyproject;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sanuth
+ */
+public class ProductTest {
+    
+    public ProductTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getProductId method, of class Product.
+     */
+    @Test
+    public void testGetProductId() {
+        System.out.println("getProductId");
+        Product instance = null;
+        String expResult = "";
+        String result = instance.getProductId();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setProductId method, of class Product.
+     */
+    @Test
+    public void testSetProductId() {
+        System.out.println("setProductId");
+        String productId = "";
+        Product instance = null;
+        instance.setProductId(productId);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getProductName method, of class Product.
+     */
+    @Test
+    public void testGetProductName() {
+        System.out.println("getProductName");
+        Product instance = null;
+        String expResult = "";
+        String result = instance.getProductName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setProductName method, of class Product.
+     */
+    @Test
+    public void testSetProductName() {
+        System.out.println("setProductName");
+        String productName = "";
+        Product instance = null;
+        instance.setProductName(productName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPrice method, of class Product.
+     */
+    @Test
+    public void testGetPrice() {
+        System.out.println("getPrice");
+        Product instance = null;
+        double expResult = 0.0;
+        double result = instance.getPrice();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setPrice method, of class Product.
+     */
+    @Test
+    public void testSetPrice() {
+        System.out.println("setPrice");
+        double price = 0.0;
+        Product instance = null;
+        instance.setPrice(price);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDiscount method, of class Product.
+     */
+    @Test
+    public void testGetDiscount() {
+        System.out.println("getDiscount");
+        Product instance = null;
+        DiscountStrategy expResult = null;
+        DiscountStrategy result = instance.getDiscount();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDiscountAmt method, of class Product.
+     */
+    @Test
+    public void testGetDiscountAmt() {
+        System.out.println("getDiscountAmt");
+        int qty = 0;
+        Product instance = null;
+        double expResult = 0.0;
+        double result = instance.getDiscountAmt(qty);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setDiscount method, of class Product.
+     */
+    @Test
+    public void testSetDiscount() {
+        System.out.println("setDiscount");
+        DiscountStrategy discount = null;
+        Product instance = null;
+        instance.setDiscount(discount);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
